2dea6e62c1ff83a6da0f5b533797d8110d40e384
alessandra martera <ale.martera@gmail.com>
2023-06-14 17:13:29 +0200

    jumbo layout background

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -43,25 +43,56 @@ export default {
     </nav>
 
     <!-- Jumbotron -->
+
+
+
+
+
+
     <div id="jumbotron">
 
-      <div class="hero_immage_left">
-        <img id="color_artist" src="hero/artist-hero-image-02-232x300.jpg" alt="">
-        <img id="monkey_artist" src="hero/artist-hero-image-01.jpg" alt="">
+      <div class="background">
 
-      </div>
+        <img class="shape-02" src="background/artist-shape-02.png" alt="">
+        <img class="shape-03" src="background/artist-shape-03.png" alt="">
+        <img class="image-03" src="background/artist-hero-image-03.png" alt="">
+        <img class="shape-01" src="background/artist-shape-01.png" alt="">
+        <img class="shape-04" src="background/artist-shape-04.png" alt="">
+        <img class="shape-05" src="background/artist-shape-05.png" alt="">
+        <img class="shape-12" src="background/maxcoach-shape-12.png" alt="">
 
-      <div class="text">
-        <div class="handwrite_blu">Hello, I'm Martin</div>
-        <h1>Aritst Coaching And Mentoring Might Be for You.</h1>
-        <button>Get started today</button>
-      </div>
+        <div class="content">
+          <!-- immage left -->
+          <div class="hero_immage_left">
+            <img id="color_artist" src="hero/artist-hero-image-02-232x300.jpg" alt="">
+            <img id="monkey_artist" src="hero/artist-hero-image-01.jpg" alt="">
+          </div>
 
-      <div class="hero_immage_right">
-        <img id="photo_artist" src="hero/artist-hero-image-04.jpg" alt="">
+          <!-- text central -->
+          <div class="text">
+            <div class="handwrite_blu">Hello, I'm Martin</div>
+            <h1>Aritst Coaching And Mentoring Might Be for You.</h1>
+            <button>Get started today</button>
+          </div>
+
+          <!-- immage right -->
+          <div class="hero_immage_right">
+            <img id="photo_artist" src="hero/artist-hero-image-04.jpg" alt="">
+          </div>
+
+          <div class="label">
+            <a href=""><i class="fa-solid fa-pen-ruler"></i></a>
+            <a href=""><i class="fa-solid fa-globe"></i></a>
+            <a href=""><i class="fa-solid fa-book"></i></a>
+            <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+
+          </div>
+
+        </div>
       </div>
 
     </div>
+
 
   </header>
 </template>
@@ -99,41 +130,119 @@ nav {
   }
 }
 
-#jumbotron {
+
+
+.background {
+  width: 100%;
+  height: 650px;
   background-color: #fbf9f6;
+  position: relative;
+
+  .shape-02 {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .shape-03 {
+    scale: 0.9;
+    position: absolute;
+    top: 100px;
+    left: -10px;
+
+  }
+
+  .image-03 {
+    position: absolute;
+    bottom: 10px;
+    left: 30%;
+  }
+
+  .shape-01 {
+    scale: 0.9;
+    position: absolute;
+    top: -38px;
+    right: 100px;
+  }
+
+  .shape-04 {
+    scale: 0.9;
+    position: absolute;
+    top: 40px;
+    right: 150px;
+  }
+
+  .shape-05 {
+    scale: 0.9;
+    position: absolute;
+    bottom: 10px;
+    right: 80px;
+  }
+
+  .shape-12 {
+    position: absolute;
+    bottom: 50px;
+    right: 350px;
+  }
+}
+
+
+
+.content {
+  position: relative;
+  width: 100%;
+  height: 650px;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 20px;
-  height: 650px;
-
 }
 
+
+
 .hero_immage_left {
+  // margin-left: 40px;
   width: 25%;
   height: 80%;
   position: relative;
 
   #color_artist {
+    width: 60%;
     position: absolute;
     top: 0;
     right: 2px;
-
+    box-shadow: $shadowHero;
   }
 
   #monkey_artist {
+    width: 40%;
     position: absolute;
     bottom: 0;
     left: 2px;
+    box-shadow: $shadowHero;
   }
 
 }
 
+.label {
+  background-color: $white;
+  font-size: 16px;
+  padding: 8px;
+  border-radius: 5px 0 0 5px;
+  position: absolute;
+  top: 60px;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  box-shadow: $shadowHero;
+
+}
+
 .text {
-  // font-family: 'Varela Round', sans-serif;
-  font-weight: 800;
-  // font-size: 16px;
-  width: 40%;
+  font-weight: 700;
+  font-size: 16px;
+  width: 30%;
   text-align: center;
 
 
@@ -148,10 +257,11 @@ nav {
   position: relative;
 
   #photo_artist {
+    width: 70%;
     position: absolute;
     bottom: 0;
-    left: 0;
-    scale: 0.9;
+    left: 50px;
+    box-shadow: $shadowHero;
   }
 }
 </style>
